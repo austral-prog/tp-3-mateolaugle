11ed112aeb1b88a223386e6e1ea45ea35091bdd7
mateolaugle <mlaugle@mail.austral.edu.ar>
2025-04-04 12:50:48 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,8 @@
 def check_vowels():
-    # Código a implementar utilizando input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+    nombre = input("ingrese nombre: ").lower()
+    print(f"contiene a: {"a" in nombre}")
+    print(f"contiene e: {"e" in nombre}")
+    print(f"contiene i: {"i" in nombre}")
+    print(f"contiene o: {"o" in nombre}")
+    print(f"contiene u: {"u" in nombre}")
+check_vowels()
