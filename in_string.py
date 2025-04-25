4d88da213e9a3a33f85c5e1c17e3842b2339df52
mateolaugle <mlaugle@mail.austral.edu.ar>
2025-04-25 12:35:56 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,8 +1,8 @@
 def check_vowels():
-    nombre = input("Ingrese su nombre: ").lower()
+    nombre = input("Ingrese su nombre: ")
+    nombre = .lower()
     print(f"Contiene a: {"a" in nombre}")
     print(f"Contiene e: {"e" in nombre}")
     print(f"Contiene i: {"i" in nombre}")
     print(f"Contiene o: {"o" in nombre}")
     print(f"Contiene u: {"u" in nombre}")
-check_vowels():
